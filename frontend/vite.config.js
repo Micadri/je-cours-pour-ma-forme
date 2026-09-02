381ab11b117ca2a18be3a0ebf0fa648f94a2ee91
Micadri <micheroux.adrien.atc@gmail.com>
2026-09-02 09:25:13 +0200

    add PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,43 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        // Force la mise en cache des audios pour courir sans réseau
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,mp3}'] 
+      },
+      manifest: {
+        name: 'Je Cours Pour Ma Forme',
+        short_name: 'JCPMF',
+        description: 'Programme d\'entraînement 5 kms',
+        theme_color: '#4CAF50',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
